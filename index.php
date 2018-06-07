6063a69f1354d5ca4a9932a65571354a377d4758
Wellington Bonfim <wellington@cappuccinodigital.com.br>
2018-06-07 13:48:52 -0300

    campo peso e e-mail adicionado

--- index.php
+++ index.php
@@ -11,19 +11,25 @@
 </head>
 <body>
     <?php 
+        //Validações
         $errors = array();
-        if(isset($_POST['acao']) == 'form'){
+        if(isset($_POST['form'])){
           
-            //array('options'=> array('min_range'=> 18),'max_range'=> 19))
             if(!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
-                $errors[] = "IDADE invalida";
+                $errors[] = 'Idade inválida';
             endif;
-            
-            
+
+            if(!$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)):
+                $errors[] = 'E-mail inválido';
+            endif;
+
+            if(!$peso = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT)):
+                $errors[] = 'Peso inválido';
+            endif;
         }
     ?>
     <h1>Validação</h1>
-    
+    <!-- Mensagens -->
     <?php if(count($errors) > 0): ?>
         <ul>
             <?php foreach($errors as $error): ?>
@@ -32,13 +38,14 @@
         </ul>
     <?php endif; ?>
 
+    <!-- Formulário -->
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-        <input type="hidden" name="acao" value="form"><br>
         Nome: <input type="text" name="nome" placeholder="Nome"><br>
         Sobrenome: <input type="text" name="sobrenome" placeholder="Sobrenome"><br>
         Idade: <input type="text" name="idade" placeholder="Idade"><br>
+        Peso: <input type="text" name="peso" placeholder="Peso"><br>
         E-mail: <input type="text" name="email" placeholde="E-mail"><br>
-        <button type="submit">Enviar</button>
+        <button type="submit" name="form">Enviar</button>
     </form>
 
 </body>
